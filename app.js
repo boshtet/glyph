f5b1f79dd724709f0934195a65980010e3cb7d09
boshtet <boshtet@protonmail.ch>
2019-06-17 02:46:51 +0800

    Command system improvements: fancy verb detection and argument cleanup

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-const Polyglot = require('node-polyglot');
-const phrases = require('./locales/en.json');
-const polyglot = new Polyglot({ phrases: phrases });
+'use strict';
 
-const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-fs.readdirSync('./commands')
+const Polyglot = require('node-polyglot');
+const phrases = require('./locales/en.json');
+const i18n = new Polyglot({ phrases: phrases });
+
+const nlp = require('compromise');
+
+const File = require('fs');
+File.readdirSync('./commands')
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
         const command = require(`./commands/${file}`);
         client.commands.set(command.name, command);
-        console.log(polyglot.t('__console_module', { module: command.name }));
-});
+        console.log(i18n.t('__console_info_module', { module: command.name }));
+    });
 
 client.on('ready', () => {
-    console.log(polyglot.t('__console_banner', { tag: client.user.tag }));
+    console.log(i18n.t('__console_info_banner', { tag: client.user.tag }));
 });
 
-client.on('message', msg => {
+client.on('message', message => {
     const self = client.user,
-        author = msg.author,
-        mentioned = msg.isMentioned(self),
-        sent_by_me = author.equals(self);
+        direct_message = message.channel.type === 'dm',
+        mentioned = message.isMentioned(self),
+        sent_by_me = message.author.equals(self),
+        for_me = (direct_message || mentioned) && !sent_by_me;
 
-    if (mentioned && !sent_by_me) {
-        const text = msg.content;
-        console.log(text);
+    if (for_me) {
+        const text = nlp(message.content.toLowerCase()).normalize(),
+            command = text.verbs().out('array')[0];
 
-        if (client.commands.has(text)) {
-            client.commands.get(text).execute(message, polyglot);
+        if (client.commands.has(command)) {
+            const mention = '<@' + self.id + '>',
+                removable = [command, mention],
+                tokens = text.words().out('array'),
+                args = tokens.filter(word => !removable.includes(word));
+            console.log(i18n.t('__console_debug_received_known_command', { command: command, args: args }));
+            client.commands.get(command).execute(message, i18n, args);
         } else {
-            msg.channel.send(polyglot.t('help_offer'));
+            console.log(i18n.t('__console_debug_received_unknown_command', { command: command }));
+            message.channel.send(i18n.t('help_offer'));
         }
     }
 });
